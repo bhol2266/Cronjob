91f11339d7525eeca4ff10d75e61499c887913ef
baldgenie <siddhartha.kapoor@gmail.com>
2022-12-20 00:38:24 +0545

    commit

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const port = 3000
 const schedule = require('node-schedule');
 const axios = require('axios');
 
-const rule = new schedule.RecurrenceRule();
-// rule.hour = 0;
-rule.minute = 35;
-// rule.tz = 'Etc/UTC';
-schedule.scheduleJob(rule, function () {
-    const url = 'https://dummyjson.com/products/1'
-    axios.get(url).then(resp => {
-        console.log(resp.data);
-    });
+// const rule = new schedule.RecurrenceRule();
+// // rule.hour = 0;
+// rule.minute = 35;
+// // rule.tz = 'Etc/UTC';
+schedule.scheduleJob('0 0 */2 * *', function () {
+    const chutlundslive_DeployHook = 'https://api.vercel.com/v1/integrations/deploy/prj_35llC1epMrjIFZMX7ympxwUXzF7P/5wF67DyvB2'
+    const desiKahani_DeployHook = 'https://api.vercel.com/v1/integrations/deploy/prj_B3rQ4A5oZTfQvkLzIKw5l5QubA6m/TedDS2ajn7'
+    const chutlundscom_DeployHook = 'https://api.vercel.com/v1/integrations/deploy/prj_Ug2Ps3DBCILSKTXGxJwrPWQgHuYF/6FDww8cuPV'
+
+    axios.get(chutlundslive_DeployHook)
+    axios.get(desiKahani_DeployHook)
+    axios.get(chutlundscom_DeployHook)
+
 });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App running on port ${port}`)
 })
