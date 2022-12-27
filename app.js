92a9123437818a3b6bb163aa57b49151a58a9ae7
baldgenie <siddhartha.kapoor@gmail.com>
2022-12-27 19:34:02 +0545

    commit

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const port = 3000
 const schedule = require('node-schedule');
 const axios = require('axios');
 
+
+
+
+
+
+var range = new schedule.Range(1, 31, 2);  // all dates, with a step of 2
+
 // const rule = new schedule.RecurrenceRule();
 // // rule.hour = 0;
 // rule.minute = 35;
 // // rule.tz = 'Etc/UTC';
-schedule.scheduleJob('0 0 */2 * *', function () {
+schedule.scheduleJob({date: range, hour: 0, minute: 0},  function () {
     const chutlundslive_DeployHook = 'https://api.vercel.com/v1/integrations/deploy/prj_35llC1epMrjIFZMX7ympxwUXzF7P/5wF67DyvB2'
     const desiKahani_DeployHook = 'https://api.vercel.com/v1/integrations/deploy/prj_B3rQ4A5oZTfQvkLzIKw5l5QubA6m/TedDS2ajn7'
     const chutlundscom_DeployHook = 'https://api.vercel.com/v1/integrations/deploy/prj_Ug2Ps3DBCILSKTXGxJwrPWQgHuYF/6FDww8cuPV'
