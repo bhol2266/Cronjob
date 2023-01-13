71d1ae435c9d4d89b2a4f6adbdcc551f229197d3
baldgenie <siddhartha.kapoor@gmail.com>
2023-01-13 22:17:50 +0545

    commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const mongoose = require("mongoose");
-
+mongoose.set("strictQuery", false);
 mongoose.connect(
     process.env.MONGODB_URI, {
     useNewUrlParser: true,
