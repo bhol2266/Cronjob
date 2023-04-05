80308ade761aee7aa7813e04f8e95bd400015656
baldgenie <siddhartha.kapoor@gmail.com>
2023-04-06 02:20:45 +0545

    commit

--- Adult Desi Kahaniya/Adult_desi_kahaniya_Notification.js
+++ Adult Desi Kahaniya/Adult_desi_kahaniya_Notification.js
@@ -9,53 +9,62 @@ const messaging = admin_Adult_DK.messaging();
 
 
 exports.Adult_desi_kahaniya_Notification = async () => {
-  const imageUrl = "https://hotdesipics.co/wp-content/uploads/2023/03/Ameer-Ladki-Aur-Gareeb-Boy-Ka-Affair-_009-150x300.jpg";
-  let finalDataArray = await singleRandomStoryForNotification()
-  let obj = finalDataArray[0];
   const db = admin_Adult_DK.database();
   const ref = db.ref(`Notification/${Date.now()}`);
+  const sendNotificationRef = db.ref('Hindi_desi_Kahani_Adult/Send_Notification');
+  sendNotificationRef.once('value', async(snapshot) => {
+    const sendNotification = snapshot.val();
 
-  ref.set({
-    Title: obj.Title,
-    Heading: obj.description.join("\n\n"),
-    Date: obj.date,
-    Notification_ImageURL: imageUrl
-  }, (error) => {
-    if (error) {
-      console.error('Error writing document', error);
-    } else {
+    if (sendNotification === "active") {
 
-      // Define the notification payload
-      const notiObject = {
-        title: obj.Title,
-        body: "पूरी कहानी पढ़ें",
-        image: imageUrl,
-        icon: "app_icon",
-      };
-
-      const extradata = {
-        KEY1: "Notification_Story",
-      };
-
-      const payload = {
-        notification: notiObject,
-        data: extradata,
-
-      };
+      const imageUrl = "https://hotdesipics.co/wp-content/uploads/2023/03/Ameer-Ladki-Aur-Gareeb-Boy-Ka-Affair-_009-150x300.jpg";
 
 
-      // Send the notification to all devices
-      messaging.sendToTopic('/topics/all', payload)
-        .then((response) => {
-          console.log('Successfully sent notification:Desi Kahani Adult', response);
-        })
-        .catch((error) => {
-          console.error('Error sending notification:', error);
-        });
+      let finalDataArray = await singleRandomStoryForNotification()
+      let obj = finalDataArray[0];
+
+
+      ref.set({
+        Title: obj.Title,
+        Heading: obj.description.join("\n\n"),
+        Date: obj.date,
+        Notification_ImageURL: imageUrl
+      }, (error) => {
+        if (error) {
+          console.error('Error writing document', error);
+        } else {
+
+          // Define the notification payload
+          const notiObject = {
+            title: obj.Title,
+            body: "पूरी कहानी पढ़ें",
+            image: imageUrl,
+            icon: "app_icon",
+          };
+
+          const extradata = {
+            KEY1: "Notification_Story",
+          };
+
+          const payload = {
+            notification: notiObject,
+            data: extradata,
+
+          };
+
+
+          // Send the notification to all devices
+          messaging.sendToTopic('/topics/all', payload)
+            .then((response) => {
+              console.log('Successfully sent notification:Desi Kahani Adult', response);
+            })
+            .catch((error) => {
+              console.error('Error sending notification:', error);
+            });
+        }
+      });
     }
   });
-
-
 
 }
 
--- Hindi Desi Kahaniya/Hindi_desi_kahaniya_Notification.js
+++ Hindi Desi Kahaniya/Hindi_desi_kahaniya_Notification.js
@@ -12,53 +12,74 @@ const messaging = admin_Hindi_DK.messaging();
 exports.Hindi_desi_kahaniya_Notification = async () => {
 
   const db = admin_Hindi_DK.database();
-  const rootRef = db.ref('Notification'); // Notification reference
-  rootRef.remove()
 
-  const imageUrl = "https://hotdesipics.co/wp-content/uploads/2023/03/Ameer-Ladki-Aur-Gareeb-Boy-Ka-Affair-_009-150x300.jpg";
-  let finalDataArray = await singleRandomStoryForNotification()
-  let obj = finalDataArray[0];
-  const ref = db.ref(`Notification/${Date.now()}`);
+  const sendNotificationRef = db.ref('Hindi_desi_Kahani-2/Send_Notification');
+  sendNotificationRef.once('value', async (snapshot) => {
+    const sendNotification = snapshot.val();
 
-  ref.set({
-    Title: obj.Title,
-    Heading: obj.description.join("\n\n"),
-    Date: obj.date,
-    Notification_ImageURL: imageUrl
-  }, (error) => {
-    if (error) {
-      console.error('Error writing document', error);
-    } else {
+    console.log(sendNotification);
+    if (sendNotification === "active") {
 
-      // Define the notification payload
-      const notiObject = {
-        title: obj.Title,
-        body: "पूरी कहानी पढ़ें",
-        image: imageUrl,
-        icon: "app_icon",
-      };
+      //remove all notification stories
+      const rootRef = db.ref('Notification'); // Notification reference
+      rootRef.remove()
 
-      const extradata = {
-        KEY1: "Notification_Story",
-      };
+      //save story to firebase notification database
+      const imageUrl = "https://hotdesipics.co/wp-content/uploads/2023/03/Ameer-Ladki-Aur-Gareeb-Boy-Ka-Affair-_009-150x300.jpg";
+      let finalDataArray = await singleRandomStoryForNotification()
+      let obj = finalDataArray[0];
+      const ref = db.ref(`Notification/${Date.now()}`);
 
-      const payload = {
-        notification: notiObject,
-        data: extradata,
+      //send notification
+      ref.set({
+        Title: obj.Title,
+        Heading: obj.description.join("\n\n"),
+        Date: obj.date,
+        Notification_ImageURL: imageUrl
+      }, (error) => {
+        if (error) {
+          console.error('Error writing document', error);
+        } else {
+    
+          // Define the notification payload
+          const notiObject = {
+            title: obj.Title,
+            body: "पूरी कहानी पढ़ें",
+            image: imageUrl,
+            icon: "app_icon",
+          };
+    
+          const extradata = {
+            KEY1: "Notification_Story",
+          };
+    
+          const payload = {
+            notification: notiObject,
+            data: extradata,
+    
+          };
+    
+    
+          // Send the notification to all devices
+          messaging.sendToTopic('/topics/all', payload)
+            .then((response) => {
+              console.log('Successfully sent notification: Hindi Desi Kahani', response);
+            })
+            .catch((error) => {
+              console.error('Error sending notification:', error);
+            });
+        }
+      });
 
-      };
-
-
-      // Send the notification to all devices
-      messaging.sendToTopic('/topics/all', payload)
-        .then((response) => {
-          console.log('Successfully sent notification: Hindi Desi Kahani', response);
-        })
-        .catch((error) => {
-          console.error('Error sending notification:', error);
-        });
     }
   });
+
+
+
+
+
+
+ 
 
 
 
