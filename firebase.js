e2340de4a984e853baeda9e0ac759dad8ba90406
bhol2266 <73753812+bhol2266@users.noreply.github.com>
2024-04-13 01:11:01 +0545

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,6 +10,8 @@ const serviceAccount_DesiGirls_VideoChat = require('./Apps Notification/DesiGirl
 const serviceAccount_DesiGirls_VideoChat2 = require('./Apps Notification/Live Desi Girls 2/serviceAccountKey.json')
 const serviceAccount_DesiGirls_VideoChat3 = require('./Apps Notification/Live Desi Girls 3/serviceAccountKey.json')
 const serviceAccount_SAXLive_VideoChat = require('./Apps Notification/SAX Live/serviceAccountKey.json')
+const serviceAccount_ChatMeet = require('./Apps Notification/ChatMeet/serviceAccountKey.json')
+const serviceAccount_ChatHub = require('./Apps Notification/ChatHub/serviceAccountKey.json')
 
 
 //multiple firebase account initialize
@@ -64,6 +66,18 @@ const saxLive_VideoChat = {
 
 };
 
+const ChatMeet = {
+    credential: admin.credential.cert(serviceAccount_ChatMeet),
+    databaseURL: 'https://live-video-chat-5-fdc3a-default-rtdb.asia-southeast1.firebasedatabase.app/',
+
+};
+
+const ChatHub = {
+    credential: admin.credential.cert(serviceAccount_ChatHub),
+    databaseURL: 'https://saxchat-2-default-rtdb.asia-southeast1.firebasedatabase.app/',
+
+};
+
 
 const admin_Adult_DK = admin.initializeApp(desikahaniAdult, 'admin_Adult_DK');
 const admin_Adult2_DK = admin.initializeApp(desikahaniAdult2, 'admin_Adult2_DK');
@@ -74,6 +88,8 @@ const admin_DesiGirls_Videochat = admin.initializeApp(desiGirls_VideoChat, 'admi
 const admin_DesiGirls_Videochat2 = admin.initializeApp(desiGirls_VideoChat2, 'admin_DesiGirls_VideoChat2');
 const admin_DesiGirls_Videochat3 = admin.initializeApp(desiGirls_VideoChat3, 'admin_DesiGirls_VideoChat3');
 const admin_SAXLive_VideoChat = admin.initializeApp(saxLive_VideoChat, 'admin_SAXLive_VideoChat');
+const admin_ChatMeet = admin.initializeApp(ChatMeet, 'admin_ChatMeet');
+const admin_ChatHub = admin.initializeApp(ChatHub, 'admin_ChatHub');
 
 module.exports = {
     admin_Adult_DK,
@@ -85,4 +101,6 @@ module.exports = {
     admin_DesiGirls_Videochat2,
     admin_DesiGirls_Videochat3,
     admin_SAXLive_VideoChat,
+    admin_ChatMeet,
+    admin_ChatHub
 };
