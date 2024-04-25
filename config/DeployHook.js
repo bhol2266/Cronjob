b0611a402981cf66e6e3d4c4c55cc1ceeb659289
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-04-25 13:57:52 +0545

    sdafsd

--- config/DeployHook.js
+++ config/DeployHook.js
@@ -4,10 +4,10 @@ const cron = require('node-cron');
 const desiKahani_DeployHook =
   "https://api.vercel.com/v1/integrations/deploy/prj_lh3yPimt3Q7HwFlod7d23pJhYGh1/2jtbPzoURw";
 const chutlundscom_DeployHook =
-"https://api.render.com/deploy/srv-cm490v21hbls73acc7hg?key=mQhbf6al7u8";
+  "https://api.render.com/deploy/srv-cm490v21hbls73acc7hg?key=mQhbf6al7u8";
 
 
-function deployFuckVideo(){
+function deployFuckVideo() {
   // cloudflare deployhook only work in post request
   const apiUrl = 'https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/67e02649-ebca-4dd6-b24c-f89fd90c1fcf';
 
@@ -18,13 +18,33 @@ function deployFuckVideo(){
       // Example: 'Authorization': 'Bearer YOUR_API_TOKEN'
     }
   })
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error.response.data);
-  });
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.response.data);
+    });
 }
+
+function deployChutlunds() {
+  // cloudflare deployhook only work in post request
+  const apiUrl = 'https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/566711ab-35cf-432a-b452-b2bde2feca96';
+
+  axios.post(apiUrl, {}, {
+    headers: {
+      'Content-Type': 'application/json',
+      // Add any required headers, such as authentication headers
+      // Example: 'Authorization': 'Bearer YOUR_API_TOKEN'
+    }
+  })
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.response.data);
+    });
+}
+
 
 
 function runDeployhooks() {
@@ -36,20 +56,12 @@ function runDeployhooks() {
           "http://desikahaniya.in/api/revalidate?secret=sadfsadfdsafdsafasdfsdafdsafsadfdsaf"
         ).catch((error) => console.log(error));
 
-        deployFuckVideo();
-      
-      axios
-        .get(
-          "https://api.render.com/deploy/srv-cm490v21hbls73acc7hg?key=mQhbf6al7u8"
-        ).catch((error) => console.log(error));
-      
+      deployFuckVideo();
+      deployChutlunds();
+
     });
 
-    // Deploy hooks cron job running every 3 days
-    cron.schedule("0 0 */3 * *", () => {
-      console.log(new Date(), "Cronjob Executed");
-      axios.get(chutlundscom_DeployHook).catch((error) => console.log(error));
-    });
+
 
   } catch (error) {
     console.error(error);
