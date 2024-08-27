a10acdb43059be3fd6786ed3e67c0760fa62aa38
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-08-27 11:24:27 +0545

    dsafsdaf

--- config/DeployHook.js
+++ config/DeployHook.js
@@ -2,62 +2,31 @@ const axios = require('axios');
 const cron = require('node-cron');
 
 const desiKahani_DeployHook =
-  "https://api.vercel.com/v1/integrations/deploy/prj_lh3yPimt3Q7HwFlod7d23pJhYGh1/2jtbPzoURw";
+  "https://www.hindisexstory.app/api/revalidate?secret=sadfsadfdsafdsafasdfsdafdsafsadfdsaf";
 const chutlundscom_DeployHook =
   "https://api.render.com/deploy/srv-cqm7899u0jms73fmnro0?key=P3rdc1kT1FE";
-
-
-function deployFuckVideo() {
-  // cloudflare deployhook only work in post request
-  const apiUrl = 'https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/67e02649-ebca-4dd6-b24c-f89fd90c1fcf';
-
-  axios.post(apiUrl, {}, {
-    headers: {
-      'Content-Type': 'application/json',
-      // Add any required headers, such as authentication headers
-      // Example: 'Authorization': 'Bearer YOUR_API_TOKEN'
-    }
-  })
-    .then(response => {
-      console.log('Response:', response.data);
-    })
-    .catch(error => {
-      console.error('Error:', error.response.data);
-    });
-}
-
-function deployChutlunds() {
-  // cloudflare deployhook only work in post request
-  axios.post(chutlundscom_DeployHook, {}, {
-    headers: {
-      'Content-Type': 'application/json',
-      // Add any required headers, such as authentication headers
-      // Example: 'Authorization': 'Bearer YOUR_API_TOKEN'
-    }
-  })
-    .then(response => {
-      console.log('Response:', response.data);
-    })
-    .catch(error => {
-      console.error('Error:', error.response.data);
-    });
-}
-
-
+const fuckvideolive_DeployHook =
+  "https://api.render.com/deploy/srv-cr619ujv2p9s73akvfv0?key=JpyMY4gvTvQ";
 
 function runDeployhooks() {
   try {
     // Revalidate cron job running daily at midnight
-    cron.schedule("0 0 * * *", () => {
-      axios
-        .get(
-          "https://www.hindisexstory.app/api/revalidate?secret=sadfsadfdsafdsafasdfsdafdsafsadfdsaf"
-        ).catch((error) => console.log(error));
-
-      deployFuckVideo();
-      deployChutlunds();
+    cron.schedule("0 0 * * *", async () => {
+      await fetch(desiKahani_DeployHook)
+      await fetch(fuckvideolive_DeployHook)
+      await fetch(chutlundscom_DeployHook)
 
     });
+
+
+    cron.schedule('0 12 * * *', async () => {
+
+      await fetch(fuckvideolive_DeployHook)
+      await fetch(chutlundscom_DeployHook)
+      await fetch(desiKahani_DeployHook)
+
+    });
+
 
 
 
