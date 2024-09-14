9375f02a31d0bdc36d8963e64b99acf3376a917b
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-09-14 22:56:12 +0545

    dfgdfsg

--- config/DeployHook.js
+++ config/DeployHook.js
@@ -2,8 +2,11 @@ const axios = require('axios');
 const cron = require('node-cron');
 
 const desiKahani_DeployHook = "https://www.hindisexstory.app/api/revalidate?secret=sadfsadfdsafdsafasdfsdafdsafsadfdsaf";
-const chutlundscom_DeployHook = "https://api.vercel.com/v1/integrations/deploy/prj_oEGWJhZ1RWP2ge98o4uihr5X6APO/waFij5v1TT";
-const fuckvideolive_DeployHook = "https://api.vercel.com/v1/integrations/deploy/prj_YUSE0fmgDMbTDx4YQ5xdFKxcePXc/wRo3dZ41mC";
+const chutlunds_revalidateHook = "https://chutlunds.com/api/revalidate?secret=sadfsadfdsafdsafasdfsdafdsafsadfdsaf";
+const fuckvideolive_revalidateHook = "https://www.fuckvideo.live/api/revalidate?secret=sadfsadfdsafdsafasdfsdafdsafsadfdsaf";
+
+const chutlundscom_DeployHook = "https://api.vercel.com/v1/integrations/deploy/prj_4YCm6CdlF3Nhix1PzrtyLh6zaPOu/EZQugZ0DVi";
+const fuckvideolive_DeployHook = "https://api.vercel.com/v1/integrations/deploy/prj_MD83WGQSbQgkBDBsqAg3s4GE0Lhy/rNrF0FU5YG";
 
 async function triggerDeployHook(url) {
   try {
@@ -18,15 +21,15 @@ function runDeployhooks() {
   // Revalidate cron job running daily at midnight
   cron.schedule("0 0 * * *", async () => {
     await triggerDeployHook(desiKahani_DeployHook);
-    await triggerDeployHook(fuckvideolive_DeployHook);
-    await triggerDeployHook(chutlundscom_DeployHook);
+    await triggerDeployHook(chutlunds_revalidateHook);
+    await triggerDeployHook(fuckvideolive_revalidateHook);
   });
 
   // Another cron job running daily at noon
   cron.schedule('0 12 * * *', async () => {
-    await triggerDeployHook(fuckvideolive_DeployHook);
-    await triggerDeployHook(chutlundscom_DeployHook);
     await triggerDeployHook(desiKahani_DeployHook);
+    await triggerDeployHook(chutlunds_revalidateHook);
+    await triggerDeployHook(fuckvideolive_revalidateHook);
   });
 }
 
